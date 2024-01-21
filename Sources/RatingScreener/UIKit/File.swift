ceb517a441a0c961eb12b64d50a7374bc75a167e
Jakob Stuber <skajake@gmail.com>
2024-01-21 08:55:48 -0600

    Initial Commit

---
+++ Sources/RatingScreener/UIKit/File.swift
@@ -0,0 +1,12 @@
+//
+//  File.swift
+//  
+//
+//  Created by Jakob Stuber on 1/21/24.
+//
+
+import SwiftUI
+
+class RatingScreenerHost : UIHostingController<ScreenerView> {
+    
+}
---
+++ Sources/RatingScreener/View/ScreenerView.swift
@@ -0,0 +1,18 @@
+//
+//  ScreenerView.swift
+//  
+//
+//  Created by Jakob Stuber on 1/21/24.
+//
+
+import SwiftUI
+
+struct ScreenerView: View {
+    var body: some View {
+        Text("Test")
+    }
+}
+
+#Preview {
+    ScreenerView()
+}
