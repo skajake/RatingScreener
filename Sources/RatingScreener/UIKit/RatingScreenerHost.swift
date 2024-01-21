afcc867f1fb05a7e8ced7369deebe7151127d116
Jakob Stuber <skajake@gmail.com>
2024-01-21 09:33:59 -0600

    host tweaks

--- Sources/RatingScreener/UIKit/RatingScreenerHost.swift
+++ Sources/RatingScreener/UIKit/RatingScreenerHost.swift
@@ -12,10 +12,17 @@ public class RatingScreenerHost : UIHostingController<ScreenerView> {
     public override init(rootView: ScreenerView) {
         super.init(rootView: rootView)
         view.backgroundColor = .clear
+        view.isOpaque = false
     }
     
     @MainActor required dynamic init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .clear
+        view.isOpaque = false
     }
     
 }
